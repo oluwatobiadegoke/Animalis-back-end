54f622e45da742d661eadb7c0727d14f5bdd7db9
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2022-02-14 18:03:33 +0100

    Like and unlike endpoint update

--- controllers/like.js
+++ controllers/like.js
@@ -5,27 +5,57 @@ const Post = require("../models/Post");
 const addLike = async (req, res) => {
   const { postId, userId } = req.body;
   if (!postId) {
-    res
+    return res
       .status(StatusCodes.BAD_REQUEST)
       .json({ success: false, msg: "postId missing." });
   }
   try {
     const post = await Post.findOne({ _id: postId });
     if (!post) {
-      res
+      return res
         .status(StatusCodes.BAD_REQUEST)
         .json({ success: false, msg: "Post not found" });
     }
-    post.comments.push({
+    post.likes.push({
       userId,
     });
     const updatedPost = await post.save();
-    res.status(StatusCodes.OK).json({ success: true, updatedPost });
+    return res.status(StatusCodes.OK).json({ success: true, updatedPost });
   } catch (error) {
-    res
+    return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ success: false, msg: "Like not updated" });
   }
 };
 
-module.exports = addLike;
+const removeLike = async (req, res) => {
+  const { postId, userId } = req.body;
+  if (!postId) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, msg: "postId missing." });
+  }
+  try {
+    const post = await Post.findOne({ _id: postId });
+    if (!post) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, msg: "Post not found" });
+    }
+    const findIndex = post.likes.findIndex((like) => like.userId === userId);
+    if (findIndex === -1) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, msg: "Like not found" });
+    }
+    post.likes.splice(findIndex, 1);
+    const updatedPost = await post.save();
+    return res.status(StatusCodes.OK).json({ success: true, updatedPost });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, msg: "Like not updated" });
+  }
+};
+
+module.exports = { addLike, removeLike };
--- routes/posts.js
+++ routes/posts.js
@@ -9,13 +9,14 @@ const {
   deletePost,
 } = require("../controllers/posts");
 
-const addLike = require("../controllers/like");
+const { addLike, removeLike } = require("../controllers/like");
 const addComment = require("../controllers/comment");
 
 router.route("/").get(getAllPosts).post(uploadPost);
 router.route("/:id").get(getPost).delete(deletePost);
 router.get("/user/posts/:id", getAllIndividualPosts);
-router.post("/like/:id", addLike);
-router.post("/comment/:id", addComment);
+router.post("/like", addLike);
+router.post("/unlike", removeLike);
+router.post("/comment", addComment);
 
 module.exports = router;
