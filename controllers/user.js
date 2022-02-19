e890043260cf485ec2fee8ac1a556b012eddd6ce
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2022-02-19 15:41:38 +0100

    user fixes

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,7 @@
 const StatusCode = require("http-status-codes");
-const cloudinary = require("cloudinary").v2;
 
 const User = require("../models/User");
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
+const Post = require("../models/Post");
 
 const getUser = async (req, res) => {
   const { id } = req.params;
@@ -35,7 +29,24 @@ const updateUser = async (req, res) => {
         .status(StatusCode.BAD_REQUEST)
         .json({ success: false, msg: "User not found" });
     }
-    const updatedUser = await User.updateOne({ _id: id }, { $set: req.body });
+    const updatedUser = await User.updateOne(
+      { _id: id },
+      { $set: req.body },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    try {
+      await Post.updateMany(
+        { userId: id },
+        { $set: { userAvatar: req.body.avatar } }
+      );
+    } catch (error) {
+      return res
+        .status(StatusCode.INTERNAL_SERVER_ERROR)
+        .json({ success: false, msg: "Something went wrong!" });
+    }
     return res.status(StatusCode.OK).json({ success: true, user: updatedUser });
   } catch (error) {
     return res
